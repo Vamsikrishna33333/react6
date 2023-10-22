075b93d44f4a1f10b5659001e68da53e3a6a7344
Vamsikrishna33333 <vk5803997@gmail.com>
2023-10-22 23:30:30 +0530

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      {/* Add your project components or details here */}
+    </section>
+  );
+}
+export default Projects;
